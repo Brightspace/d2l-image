594ebabf38f255965c413c8997e7dcb1bf4d6221
James Klassen <James.Klassen@D2L.com>
2022-08-17 11:06:17 -0500

    Add tests for fetch responses and events

--- test/d2l-image.test.js
+++ test/d2l-image.test.js
@@ -1,9 +1,84 @@
 import '../d2l-image.js';
-import { expect, fixture, html } from '@open-wc/testing';
+import { expect, fixture, html, oneEvent } from '@open-wc/testing';
+import { stub } from 'sinon';
 
 describe('d2l-image', () => {
+	let fetchStub;
+
+	beforeEach(() => {
+		fetchStub = stub(window, 'fetch');
+	});
+
+	afterEach(() => {
+		fetchStub.restore();
+	});
+
 	it('loads element', async() => {
 		const el = await fixture(html`<d2l-image></d2l-image>`);
 		expect(el).to.exist;
+	});
+
+	it('does not render an <img> without image url', async() => {
+		const el = await fixture(html`<d2l-image></d2l-image>`);
+		expect(el.shadowRoot.querySelector('img')).to.not.exist;
+	});
+
+	describe('Hypermedia calls', () => {
+
+		beforeEach(() => {
+			fetchStub.resolves({ ok: true });
+		});
+
+		describe('Sending requests', () => {
+
+			it('should not send a request without an image url', async() => {
+				const el = await fixture(html`<d2l-image></d2l-image>`);
+
+				expect(el.imageUrl).to.be.undefined;
+				expect(fetchStub).to.not.have.been.called;
+			});
+
+			it('should send a request with an image url', async() => {
+				const el = await fixture(html`<d2l-image image-url="image.jpg"></d2l-image>`);
+
+				expect(el.imageUrl).to.equal('image.jpg');
+				expect(fetchStub).to.have.been.called;
+			});
+		});
+
+		describe('Receiving responses', () => {
+
+			it('should update image src attribute and fire image-loaded event if request was successful', async() => {
+				const response = new Response(new Blob(['image'], { type: 'image/jpeg' }));
+				fetchStub.resolves(response);
+
+				const el = await fixture(html`<d2l-image></d2l-image>`);
+
+				el.imageUrl = 'image4.jpg';
+
+				const { detail } = await oneEvent(el, 'd2l-image-loaded');
+				expect(detail.response).to.equal(response);
+
+				const imgElement = el.shadowRoot.querySelector('img');
+				expect(imgElement).to.exist;
+				expect(imgElement.src).to.have.string('blob:http://localhost:');
+			});
+
+			it('should fire a failed-to-load event if the request was not successful', async() => {
+				const response = { status: 404 };
+				fetchStub.resolves(response);
+
+				const el = await fixture(html`<d2l-image></d2l-image>`);
+
+				el.imageUrl = 'none.jpg';
+
+				const { detail } = await oneEvent(el, 'd2l-image-failed-to-load');
+				expect(detail.response).to.equal(response);
+
+				const imgElement = el.shadowRoot.querySelector('img');
+				expect(imgElement).to.exist;
+				expect(imgElement.src).to.have.string('none.jpg');
+			});
+		});
 	});
 });
