2ccd0ec58c41479443adefff646d6ce3bb6b4dfa
James <13937038+jameswklassen@users.noreply.github.com>
2022-08-18 13:39:01 -0500

    feat: Convert to Lit (#31)

--- d2l-image.js
+++ d2l-image.js
@@ -1,81 +1,111 @@
-import '@polymer/polymer/polymer-legacy.js';
-import '@polymer/iron-ajax/iron-ajax.js';
-import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
-const $_documentContainer = document.createElement('template');
+import { css, html, LitElement, nothing } from 'lit';
+import { ifDefined } from 'lit/directives/if-defined.js';
 
-$_documentContainer.innerHTML = `<dom-module id="d2l-image">
-	<template strip-whitespace="">
-		<style>
+class D2LImage extends LitElement {
+	static get properties() {
+		return {
+			/**
+			 * Alternate text describing the image
+			 * @type {string}
+			 */
+			alternateText: { type: String, attribute: 'alternate-text' },
+			/**
+			 * URL of the image
+			 * @type {string}
+			 */
+			imageUrl: { type: String, attribute: 'image-url' },
+			/**
+			 * token used to authenticate the image retrieval
+			 * @type {string}
+			 */
+			token: { type: String },
+			_imageUrl: { state: true }
+		};
+	}
+
+	static get styles() {
+		return css`
 			:host {
 				display: block;
 			}
 
 			img {
-				width:100%;
-				height: 100%;
 				border-radius: var(--d2l-image-border-radius);
+				height: 100%;
 				object-fit: var(--d2l-image-object-fit);
+				width: 100%;
 			}
-		</style>
+		`;
+	}
 
-		<iron-ajax id="imageRequest" url="[[imageUrl]]" headers="[[_headers]]" on-iron-ajax-response="_onImageResponse" on-iron-ajax-error="_onImageResponse" handle-as="blob">
-		</iron-ajax>
+	constructor() {
+		super();
+		this.imageUrl = '';
+		this.alternateText = '';
+		this.token = undefined;
+	}
 
-		<img id="image" alt$="[[alternateText]]">
-	</template>
-
-</dom-module>`;
-
-document.head.appendChild($_documentContainer.content);
-
-Polymer({
-	is: 'd2l-image',
-
-	properties: {
-		alternateText: {
-			type: String,
-			reflectToAttribute: true,
-		},
-		imageUrl: {
-			type: String,
-			value: null
-		},
-		token: String,
-		_headers: String
-	},
-
-	observers: [
-		'_onImageUrlChange( imageUrl, token )'
-	],
-
-	_onImageUrlChange: function(imageUrl, token) {
-		if (imageUrl && token) {
-			this._headers = {
-				Authorization: 'Bearer ' + token
-			};
-			this.$.imageRequest.generateRequest();
-		} else if (imageUrl) {
-			this.$.image.src = imageUrl;
+	render() {
+		if (!this._imageUrl) {
+			return nothing;
 		}
-	},
 
-	_onImageResponse: function(response) {
-		if (response.detail.status === 200) {
-			if (this.$.image.src) {
-				URL.revokeObjectURL(this.$.image.src);
+		return html`
+			<img alt="${this.alternateText}" src="${ifDefined(this._imageUrl)}">
+		`;
+	}
+
+	updated(changedProperties) {
+		if (changedProperties.has('imageUrl') || changedProperties.has('token')) {
+			this._loadImage();
+		}
+	}
+
+	async _loadImage() {
+		if (!this.imageUrl) {
+			return;
+		}
+
+		if (!this.token) {
+			this._imageUrl = this.imageUrl;
+			return;
+		}
+
+		let response;
+
+		try {
+			const tokenPromise = await (typeof (this.token) === 'function')
+				? this.token()
+				: Promise.resolve(this.token);
+
+			const tokenString = await tokenPromise;
+
+			const headers = new Headers();
+			if (tokenString) {
+				headers.append('Authorization', `Bearer ${tokenString}`);
 			}
-			this.$.image.src = URL.createObjectURL(response.detail.response);
+
+			response = await window.fetch(this.imageUrl, { method: 'GET', headers });
+
+			const blob = await response.blob();
+			this._imageUrl = URL.createObjectURL(blob);
+
 			this.dispatchEvent(new CustomEvent('d2l-image-loaded', {
-				bubbles: true,
-				composed: true,
-				detail: { response: response }
+				bubbles: false,
+				composed: false,
+				detail: { response }
 			}));
-		} else {
+
+		} catch (e) {
+			this._imageUrl = this.imageUrl;
+
 			this.dispatchEvent(new CustomEvent('d2l-image-failed-to-load', {
-				bubbles: true,
-				composed: true,
-				detail: { response: response }
+				bubbles: false,
+				composed: false,
+				detail: { response }
 			}));
 		}
 	}
-});
+}
+
+customElements.define('d2l-image', D2LImage);
